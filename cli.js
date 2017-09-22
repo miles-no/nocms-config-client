77e5fe443f102659970ac5cf8f1de88f2eaf737e
Jørgen Landsnes <jorgen@landsn.es>
2017-09-22 15:11:32 +0200

    util: Enabled supprt for sending key as fourth argument to cli.js

--- cli.js
+++ cli.js
@@ -1,17 +1,17 @@
 const AES256 = require('./crypto/algorithms/aes256');
 const operation = process.argv[2];
 
+const key = process.argv.length === 5 ? process.argv[3] : process.env.CRYPTEX_KEYSOURCE_PLAINTEXT_KEY;
+
 const operations = {
   encrypt: (secret) => {
     const algo = new AES256();
-    const key = process.env.CRYPTEX_KEYSOURCE_PLAINTEXT_KEY;
 
     console.log(algo.encrypt(key, secret));
   },
 
   decrypt: (secret) => {
     const algo = new AES256();
-    const key = process.env.CRYPTEX_KEYSOURCE_PLAINTEXT_KEY;
 
     console.log(algo.decrypt(key, secret));
   },
@@ -22,5 +22,4 @@ if (!operations[operation]) {
   process.exit(9);
 }
 
-const operationArgs = process.argv.slice(3);
-operations[operation](...operationArgs);
+operations[operation](process.argv[process.argv.length - 1]);
